aa291b3e905dc6ec972412f9f4c056ec2162be80
youseefmoemen <55257820+youseefmoemen@users.noreply.github.com>
2021-07-18 14:32:40 +0200

    fixing bugs

--- main.cpp
+++ main.cpp
@@ -75,6 +75,7 @@ int main()
                 while (tmp2 and flag2) {
                     if (tmp2->user->data_userName == b) {
                         tmp->friends->insertNode(tmp->friends, tmp2->user);
+                        tmp2->friends->insertNode(tmp2->friends, tmp->user);
                         flag2 = false;
                     }
                     tmp2 = tmp2->next;
